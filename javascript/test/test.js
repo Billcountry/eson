16630016f62bfb1123222a3c2a859bdf0a7cb5ee
Billcountry <me@billcountry.tech>
2020-05-06 20:11:33 +0300

    ESON Tests

---
+++ javascript/test/test.js
@@ -0,0 +1,109 @@
+const assert = require("assert")
+const eson = require("../index")
+
+describe("Test Normal JSON Operations", () => {
+    it("Should encode the same way as Javascript built in JSON", () => {
+        const object = {
+            name: "Jane Doe",
+            sibling: "John Doe",
+        }
+        assert.equal(eson.encode(object), JSON.stringify(object))
+    })
+
+    it("Should be able to decode JSON meant for built in JSON", () => {
+        const json_data = '{"name": "Jane Doe"}'
+        assert.equal(eson.decode(json_data), JSON.parse(json_data))
+    })
+
+    it("Should return pretty JSON equal to built in JSON", () => {
+        const data = {
+            name: "Jane Doe",
+            registered: new Date(2020, 5, 7, 6, 30, 40, 400),
+        }
+        eson_data = {
+            name: "Jane Doe",
+            "EsonDatetime~registered": { timestamp: 1588822240400000 },
+        }
+        assert.equal(
+            eson.encode(data, true),
+            JSON.stringify(eson_data, null, 4)
+        )
+    })
+})
+
+describe("Test List Operations", () => {
+    it("Should support backward array compartibility", () => {
+        const some_json_array = "[1, 2, 3]"
+        assert.equal(eson.decode(some_json_array), [1, 2, 3])
+    })
+
+    it("Should encode an array to an eson list object", () => {
+        const num_list = [1, 2, 3]
+        const expected_eson = '{"__eson-list__": {"0": 1, "1": 2, "2": 3}}'
+        assert.equal(eson.encode(num_list), expected_eson)
+    })
+
+    it("Should decode an eson list object to a normal JS array", () => {
+        const eson_data =
+            '{"__eson-list__": {"0": 1, "1": 2, "2": 3, "3": "Some String"}}'
+        const expected_array = [1, 2, 3, "Some String"]
+        assert.equal(eson.decode(eson_data), expected_array)
+    })
+})
+
+describe("Test Datetime Operations", () => {
+    it("Should decode EsonDate object to a javascript date object", () => {
+        const eson_data =
+            '{"EsonDate~dob": {"year": 2020, "month": 4, "day": 20}, "name": "Corona"}'
+        const expected_object = {
+            dob: new Date(2020, 3, 20),
+            name: "Corona",
+        }
+        assert.equal(eson.decode(eson_data), expected_object)
+    })
+
+    it("Should encode Javascript Date object to EsonDatetime object", () => {
+        const data = {
+            registered: new Date(2020, 4, 7, 6, 30, 40, 400),
+            username: "bear",
+        }
+        const expected_eson =
+            '{"EsonDatetime~registered": {"timestamp": 1588822240400000}, "username": "bear"}'
+        assert.equal(eson.encode(data), expected_eson)
+    })
+
+    it("Should decode EsonDatetime object to a javascript date object", () => {
+        const eson_data =
+            '{"EsonDatetime~registered": {"timestamp": 1588822240400000}, "username": "fox"}'
+        const expected_object = {
+            registered: new Date(2020, 4, 7, 6, 30, 40, 400),
+            username: "bear",
+        }
+        assert.equal(eson.decode(eson_data), expected_object)
+    })
+})
+
+describe("Test Combined List Data", () => {
+    it("Should encode data in lists and dictionaries correctly", () => {
+        const dt = new Date(2020, 5, 7, 6, 30, 40, 400)
+        const data = {
+            name: "Jane Doe",
+            log: ["Some string", 0, dt, false, null],
+        }
+        const expected_eson =
+            '{"name": "Jane Doe", "log": {"__eson-list__": {"0": "Some string", "1": 0, "EsonDatetime~2": {"timestamp": 1588822240400000}, "3": false, "4": null}}}'
+        assert.equal(eson.encode(data), expected_eson)
+    })
+
+    it("Shoud decode data in lists and dictionaries correctly", () => {
+        const eson_string =
+            '{"name": "Jane Doe", "log": {"__eson-list__": {"0": "Some string", "1": 0, "EsonDatetime~2": {"timestamp": 1588822240400000}, "3": false, "EsonDate~4": {"year": 2020, "month": 4, "day": 20}, "5": null}}}'
+        const datetime = new Date(2020, 5, 7, 6, 30, 40, 400)
+        const date = new Date(2020, 3, 20)
+        const expected_object = {
+            name: "Jane Doe",
+            log: ["Some string", 0, datetime, false, date, null],
+        }
+        assert.equal(eson.decode(eson_string), expected_object)
+    })
+})
