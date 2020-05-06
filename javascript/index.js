7600f27b85afffe05728e5fce4c939a901333044
Billcountry <me@billcountry.tech>
2020-05-06 18:30:28 +0300

    Code skeleton

---
+++ javascript/index.js
@@ -0,0 +1,47 @@
+export const encode = value => {
+
+}
+
+export const decode = value => {
+
+}
+
+export const add_extension = extension => {
+    if(typeof extension.should_encode !== "function"){
+        throw new Error("Extension must provide a function, `should_encode`")
+    }
+    if(typeof extension.encode !== "function"){
+        throw new Error("Extension must provide a function, `encode`")
+    }
+    if(typeof extension.decode !== "function"){
+        throw new Error("Extension must provide a function, `decode`")
+    }
+    if(!extension.name || typeof extension.name !== "string"){
+        throw new Error("Extension must provide a 'name' to identify the extension data type")
+    }
+    const config = get_config()
+    config[extension.name] = extension
+    global_object().__eson_config__ = config
+}
+
+const global_object = () => {
+    try{
+        return global
+    }catch(err){}
+
+    try{
+        return window
+    }catch(err){}
+
+    return self
+}
+
+const get_config = ()=>{
+    global_object().__eson_config__ || {}
+}
+
+const eson = {
+    encode, decode, add_extension
+}
+
+export default eson
