9fb5db19e6a1c454b016905d74e27fa1473367ba
Billcountry <me@billcountry.tech>
2020-05-07 10:01:48 +0300

    Encode datetime correctly

--- javascript/extensions/datetime.js
+++ javascript/extensions/datetime.js
@@ -3,7 +3,9 @@ exports.EsonDatetime = {
     should_encode: value => value instanceof Date,
     encode: value => {
         // ESON processes datetime in micro-seconds
-        return value.getTime() * 1000
+        return {
+            timestamp: value.getTime() * 1000,
+        }
     },
     decode: value => new Date(value.timestamp / 1000),
 }
