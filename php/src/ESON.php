8b203dcad866be0c8ad99e82e0153b942a4ff713
Billcountry <me@billcountry.tech>
2020-05-15 19:37:36 +0300

    Encoding logic

--- php/src/ESON.php
+++ php/src/ESON.php
@@ -16,31 +16,74 @@ class ESON
         if($pretty){
             return json_encode($data, JSON_PRETTY_PRINT);
         }
-        return json_encode($eson_data);
+        return json_encode($data);
     }
 
     private static function encode_types($data){
         if(is_array($data) && self::is_assoc($data)){
             $eson_array = array();
             foreach ($data as $key => $value) {
-                
+                $result = self::encode_type($key, $value);
+                $encoded_key = $result["encoded_key"];
+                $encoded_value = $result["encoded_value"];
+                if(is_array($encoded_value)){
+                    $encoded_value = self::encode_types($data);
+                }
+                $eson_array[$encoded_key] = $encoded_value;
+            }
+            return $eson_array;
+        }
+        if(is_array($data)){
+            $eson_array = array();
+            foreach($data as $value){
+                $result = self::encode_type("", $value);
+                $encoded_key = $result["encoded_key"];
+                $encoded_value = $result["encoded_value"];
+                if(is_array($encoded_value)){
+                    $encoded_value = self::encode_types($data);
+                }
+                if($encoded_key){
+                    array_push($eson_array, array($encoded_key => $encoded_value));
+                }else{
+                    array_push($eson_array, $encoded_value);
+                }
+            }
+            return $eson_array;
+        }
+        $result = self::encode_type("", $data);
+        $encoded_key = $result["encoded_key"];
+        $encoded_value = $result["encoded_value"];
+        if(is_array($encoded_value)){
+            $encoded_value = self::encode_types($data);
+        }
+        if($encoded_key){
+            return array($encoded_key => $encoded_value);
+        }
+        return $data;
+    }
+
+    private static function encode_type($key, $value){
+        $config = self::config();
+        foreach ($config as $name => $extension){
+            if($extension["should_encode"]($value)){
+                return array(
+                    "encoded_key" => "$name~$key",
+                    "encoded_value" => $extension["encode"]($value)
+                );
             }
         }
+        return array("encoded_key" => $key, "encoded_value" => $value);
     }
 
-    private static function encode_type($value){
-        
-    }
-
-    public static function decode($eson_data){
-
+    public static function decode($eson_string){
+        return json_decode($eson_string, true);
     }
 
     public static function add_extension(array $extension){
         if(!isset($extension["name"])
             || !isset($extension["encode"])
             || !isset($extension["decode"])
-            || !isset($extension["name"])){
+            || !isset($extension["should_encode"])){
 
             throw new Exception("An eson extension must provide the following"
                                 ." 'name'->string,"
@@ -61,6 +104,6 @@ class ESON
     }
 
     private static function config(){
-        global $_ESON_CONFIG;
+        return $GLOBALS['_ESON_CONFIG'];
     }
 }
