b97910648595c3c6d5d6638b03e899d2cafcd6ca
Billcountry <me@billcountry.tech>
2020-05-20 19:18:28 +0300

    Optimize javascript code

--- javascript/encoder.js
+++ javascript/encoder.js
@@ -13,13 +13,6 @@ const eson_encode = (config, value) => {
                 "",
                 element
             )
-            if (
-                encoded_value &&
-                (Array.isArray(encoded_value) ||
-                    encoded_value.constructor == Object)
-            ) {
-                encoded_value = eson_encode(config, encoded_value)
-            }
             if (encoded_key) {
                 const encoded_object = {}
                 encoded_object[encoded_key] = encoded_value
@@ -37,25 +30,12 @@ const eson_encode = (config, value) => {
                 key,
                 value
             )
-            if (
-                encoded_value &&
-                (Array.isArray(encoded_value) ||
-                    encoded_value.constructor == Object)
-            ) {
-                encoded_value = eson_encode(config, encoded_value)
-            }
             eson_data[encoded_key] = encoded_value
         })
         return eson_data
     }
 
     let [encoded_key, encoded_value] = eson_encode_type(config, "", value)
-    if (
-        encoded_value &&
-        (Array.isArray(encoded_value) || encoded_value.constructor == Object)
-    ) {
-        encoded_value = eson_encode(config, encoded_value)
-    }
     if (encoded_key) {
         const encoded_object = {}
         encoded_object[encoded_key] = encoded_value
@@ -65,14 +45,21 @@ const eson_encode = (config, value) => {
 }
 
 const eson_encode_type = (config, key, value) => {
+    let [encoded_key, encoded_value] = [key, value]
     let name
     for (name in config) {
         const extension = config[name]
         if (extension.should_encode(value)) {
-            const encoded_key = `${extension.name}~${key}`
-            const encoded_value = extension.encode(value)
-            return [encoded_key, encoded_value]
+            encoded_key = `${extension.name}~${key}`
+            encoded_value = extension.encode(value)
+            break
         }
     }
-    return [key, value]
+    if (
+        encoded_value &&
+        (Array.isArray(encoded_value) || encoded_value.constructor == Object)
+    ) {
+        encoded_value = eson_encode(config, encoded_value)
+    }
+    return [encoded_key, encoded_value]
 }
