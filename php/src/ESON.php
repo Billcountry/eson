c3a98e189d112e5d42a63ffd14985c9f34f35c91
Billcountry <me@billcountry.tech>
2020-05-20 20:09:55 +0300

    Decoding logic

--- php/src/ESON.php
+++ php/src/ESON.php
@@ -53,20 +53,66 @@ class ESON
     private static function encode_type($key, $value)
     {
         $config = self::config();
+        $encoded_key = $key;
+        $encoded_value = $value;
         foreach ($config as $name => $extension) {
             if ($extension["should_encode"]($value)) {
-                return array(
-                    "encoded_key" => "$name~$key",
-                    "encoded_value" => $extension["encode"]($value)
-                );
+                $encoded_key = "$name~$key";
+                $encoded_value = $extension["encode"]($value);
+                break;
             }
         }
-        return array("encoded_key" => $key, "encoded_value" => $value);
+        if(is_array($encoded_value)){
+            $encoded_value = self::encode_types($encoded_value);
+        }
+        return array("encoded_key" => $encoded_key, "encoded_value" => $encoded_value);
     }
 
     public static function decode($eson_string)
     {
-        return json_decode($eson_string, true);
+        $eson_data = json_decode($eson_string, true);
+        return self::decode_types($eson_data);
+    }
+
+    private static function decode_types($data)
+    {
+        if(is_array($data) && self::is_assoc($data)){
+            $eson_array = array();
+            foreach($data as $encoded_key => $encoded_value){
+                extract(self::decode_type($encoded_key, $encoded_value));
+                if(is_array($value)){
+                    $value = self::decode_types($value);
+                }
+                if(!$key){
+                    return $value;
+                }
+                $eson_array[$key] = $value;
+            }
+            return $eson_array;
+        }
+        if(is_array($data)){
+            $eson_array = array();
+            foreach($data as $value){
+                if(is_array($value)){
+                    $value = self::decode_types($value);
+                }
+                array_push($eson_array, $value);
+            }
+            return $eson_array;
+        }
+        return $data;
+    }
+
+    private static function decode_type($encoded_key, $encoded_value){
+        $key_parts = explode("~", $encoded_key);
+        $config = self::config();
+        if(count($key_parts) == 2 && isset($config[$key_parts[0]]) ){
+            return array(
+                "key" => $key_parts[1],
+                "value" => $config[$key_parts[0]]["decode"]($encoded_value)
+            );
+        }
+        return array("key" => $encoded_key, "value" => $encoded_value);
     }
 
     public static function add_extension(array $extension)
@@ -99,7 +145,7 @@ class ESON
         if (!isset($GLOBALS['_ESON_CONFIG'])) {
             $GLOBALS['_ESON_CONFIG'] = array();
             // Add default extensions
-            ESON::add_extension(array(
+            self::add_extension(array(
                 "name" => "EsonDatetime",
                 "should_encode" => function ($value) {
                     return $value instanceof DateTime;
@@ -114,7 +160,7 @@ class ESON
                     return $dt;
                 }
             ));
-            ESON::add_extension(array(
+            self::add_extension(array(
                 "name" => "EsonDate",
                 // Should only decode not encode
                 "should_encode" => function ($value) {
